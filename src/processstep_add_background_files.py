53a6e67d147a30ab9622d68c157cd4009427653e
toqduj <brian.pauw@bam.de>
2025-11-03 11:50:49 +0100

    missing absorption_bg

--- src/processstep_add_background_files.py
+++ src/processstep_add_background_files.py
@@ -1,17 +1,16 @@
 from pathlib import Path
-import subprocess
 from typing import Tuple, Union
 
 import h5py
-from YMD_class import YMD, extract_metadata_from_path
+from YMD_class import extract_metadata_from_path
 from defaults_carrier import DefaultsCarrier
-from logbook2mouse.logbook_reader import Logbook2MouseReader
+from logbook2mouse.logbook_reader import Logbook2MouseReader  # type: ignore
 import logging
 
 from utilities import get_configuration
 
 doc = """
-This processing step adds the stacked background files (whether they exist or not) for this measurement. 
+This processing step adds the stacked background files (whether they exist or not) for this measurement.
 Should be run after the metadata update step.
 """
 
@@ -44,8 +43,10 @@ def get_background_identifiers(filename: Path, logger: logging.Logger) -> Tuple[
         bgid = None
         dbgid = None
 
-    if bgid == 'None': bgid = None
-    if dbgid == 'None': dbgid = None
+    if bgid == 'None':
+        bgid = None
+    if dbgid == 'None':
+        dbgid = None
 
     return bgid, dbgid
 
@@ -60,7 +61,7 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
         logger.info(f"Starting background adder for {input_file}")
         config = get_configuration(input_file, logger)
         bg_id, dbg_id = get_background_identifiers(input_file, logger)
-        # write result to HDF5 file: 
+        # write result to HDF5 file:
         with h5py.File(input_file, 'a') as h5f:
             bg_file_dataset = h5f.require_dataset('/entry1/processing_required_metadata/background_file', shape=(), dtype=h5py.special_dtype(vlen=str))
             rel_destination = ''
@@ -68,6 +69,7 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
                 destination = defaults.data_dir / bg_id[0:4] / bg_id[0:8] / f'MOUSE_{bg_id}_{config}_stacked.nxs'
                 rel_destination = destination.relative_to(input_file.parent, walk_up=True)
             bg_file_dataset[...] = str(rel_destination)
+            bg_file_dataset.attrs.update({'note': 'background filename added by MOUSEDataPipeline, relative to preprocessed, stacked, file'})
             # bg_file_dataset[...] = str(Path('..', '..') / bg_id[0:4] / bg_id[0:8] / f'MOUSE_{bg_id}_{config}_stacked.nxs') if bg_id else ''
             dbg_file_dataset = h5f.require_dataset('/entry1/processing_required_metadata/dispersed_background_file', shape=(), dtype=h5py.special_dtype(vlen=str))
             rel_destination = ''
@@ -75,10 +77,12 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
                 destination = defaults.data_dir / dbg_id[0:4] / dbg_id[0:8] / f'MOUSE_{dbg_id}_{config}_stacked.nxs'
                 rel_destination = destination.relative_to(input_file.parent, walk_up=True)
             dbg_file_dataset[...] = str(rel_destination)
+            dbg_file_dataset.attrs.update({'note': 'dispersed background filename added by MOUSEDataPipeline, relative to preprocessed, stacked, file'})
+
             # dbg_file_dataset[...] = str(Path('..', '..') / dbg_id[0:4] / dbg_id[0:8] / f'MOUSE_{dbg_id}_{config}_stacked.nxs') if dbg_id else ''
         logger.info(f"Completed background adder step for {input_file}")
     except Exception as e:
         # Print the standard output and standard error
-        logger.info(f"Processstep processstep_add_background_files failed with stderr:")
+        logger.info("Processstep processstep_add_background_files failed with stderr:")
         logger.info(e)
         logger.error(f"Error during background adder subprocess: {e}")
--- src/processstep_thickness_from_absorption.py
+++ src/processstep_thickness_from_absorption.py
@@ -1,20 +1,19 @@
 from pathlib import Path
-import subprocess
 from typing import Union
 
 import h5py
 import numpy as np
 from YMD_class import extract_metadata_from_path
 from defaults_carrier import DefaultsCarrier
-from logbook2mouse.logbook_reader import Logbook2MouseReader
+from logbook2mouse.logbook_reader import Logbook2MouseReader  # type: ignore
 import logging
-from HDF5Translator.translator_elements import TranslationElement, LinkElement
-from HDF5Translator.translator import process_translation_element, process_link_element
+from HDF5Translator.translator_elements import TranslationElement  # type: ignore
+from HDF5Translator.translator import process_translation_element  # type: ignore
 from utilities import get_float_from_h5, get_str_from_h5
 
 doc = """
-This processing step updates the metadata with the estimated thickness from the 
-X-ray absorption and the X-ray absorption coefficient calculated from the composition. 
+This processing step updates the metadata with the estimated thickness from the
+X-ray absorption and the X-ray absorption coefficient calculated from the composition.
 """
 
 # Flag indicating whether this process step can be executed in parallel on multiple repetitions
@@ -44,7 +43,7 @@ def calculate_thickness(absorption_coefficient: float, absorption: float, logger
     # we cannot deal with absorption > 1, which can happen due to noise in the transmission measurement for extremely low transmissions (e.g. very thick samples). This would make np.log(1-abs) imaginary
 
     if absorption_coefficient == 0:
-        logger.warning(f'absorption coefficient is zero, cannot calculate thickness')
+        logger.warning('absorption coefficient is zero, cannot calculate thickness')
         return -1
     if not (0 < abs_val <= 1):
         logger.warning(f'absorption value {abs_val} is not in the range [0, 1]')
@@ -61,10 +60,12 @@ def get_background_file(filename: Path, logger: logging.Logger) -> Union[Path, N
     # make it relative to the current file
     if background_file:
         background_file = (filename.parent / background_file).resolve()
-    # print(f' * * * Found background file {background_file} for sample file {filename}')
+        logging.info(f' * * * background file {background_file=} specified for sample file {filename=}')
     if background_file and Path(background_file).is_file():
+        logging.info('background file exists')
         return Path(background_file)
     else:
+        logging.info(f' * * * Did not find {background_file=} for {filename=}')
         return None
 
 
@@ -85,7 +86,7 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
             return
         transmission = get_float_from_h5(input_file, HDFPath='/entry1/sample/transmission', logger=logger)
         absorption = 1 - transmission
-        # we also need to get the absorption from the background file if it exists: 
+        # we also need to get the absorption from the background file if it exists:
         background_file = get_background_file(input_file, logger)
 
         # get the transmission from the background file if we can, to later isolate the absorption from the sample only
@@ -101,18 +102,16 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
             else:
                 # we can get the mean from the stacked background file. transmission will be meaned if it is an array by get_float_from_h5
                 transmission_bg = get_float_from_h5(background_file, HDFPath='/entry1/sample/transmission', logger=logger)
+                absorption_bg = 1 - transmission_bg
                 # here we assume that the background absorption is only due to the container
-                transmission_sample = transmission / transmission_bg  # if transmission_bg >= 0 else transmission # nope, have to account for noise and rely on averaging. 
+                transmission_sample = transmission / transmission_bg  # if transmission_bg >= 0 else transmission # nope, have to account for noise and rely on averaging.
                 absorption_sample = 1-transmission_sample
-                # if not (0 < absorption_sample < 1):
-                #     logger.warning(f"Sample-specific absorption {absorption_sample} outside of realistic limits. total absorption: {absorption}, background absorption: {absorption_bg}. resetting to {absorption}")
-                #     absorption_sample = absorption
 
         # Calculate the thickness from the absorption data
         thickness = calculate_thickness(absorption_coefficient, absorption_sample, logger)
         logging.info(f'Calculated {thickness=:0.03e} m from {absorption=:0.03e} ({absorption_sample=:0.03e} and background absorption {absorption_bg=:0.03e} from {background_file=}) and {absorption_coefficient=:0.03e} 1/m for file {input_file})')
-
-        # Now let's store all that information in the HDF5 file. 
+        print(f'Calculated {thickness=:0.03e} m from {absorption=:0.03e} ({absorption_sample=:0.03e} and background absorption {absorption_bg=:0.03e} from {background_file=}) and {absorption_coefficient=:0.03e} 1/m for file {input_file})')
+        # Now let's store all that information in the HDF5 file.
         TElements = []  # we want to add two elements, so I make a list
         TElements += [
             TranslationElement(
@@ -187,11 +186,6 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
             )]
             # "/entry1 /sample/absorptionDerivedThickness"
 
-            # LE = LinkElement(
-            #     source_path="/entry1/sample/absorptionDerivedThickness",
-            #     destination_path="/entry1/sample/thickness",
-            #     soft_or_hard_link="hard",
-            # )
         else:
             logger.info(f'keeping logbook-specified samplethickness {samplethickness} since it was positive')
             TElements += [TranslationElement(
@@ -206,7 +200,7 @@ def run(dir_path: Path, defaults: DefaultsCarrier, logbook_reader: Logbook2Mouse
                     "note": "Set to logbook-specified samplethickness since it was positive. Determined by the processstep_thickness_from_absorption post-translation processing script."
                 },
             )]
-        
+
         # writing the resulting metadata back to the main HDF5 file
         with h5py.File(input_file, "r+") as h5_out:
             for element in TElements:  # iterate over the two elements and write them back
